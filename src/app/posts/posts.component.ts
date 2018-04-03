3c0f9552fb7a69a1b30a5d46b4cbc4eaf8e51690
dmaddox <dmaddox@gmail.com>
2018-04-02 22:28:46 -0500

    patch request

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -12,18 +12,26 @@ export class PostsComponent {
 
   constructor(private http: Http) {
     http.get(this.url)
-    .subscribe(response => {
-      this.posts = response.json();
-    };)
+      .subscribe(response => {
+        this.posts = response.json();
+      };)
   }
 
   createPost(input: HTMLInputElement) {
     let post = { title: input.value };
     input.value = '';
-    this.http.post(this.url, JSON.stringify(post)).subscribe(response => {
-      post['id'] = response.json().id;
-      this.posts.splice(0, 0, post);
-    });
+    this.http.post(this.url, JSON.stringify(post))
+      .subscribe(response => {
+        post['id'] = response.json().id;
+        this.posts.splice(0, 0, post);
+      });
+  }
+
+  updatePost(post) {
+    this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }))
+      .subscribe(response => {
+        console.log(response.json());
+    })
   }
 
 }
